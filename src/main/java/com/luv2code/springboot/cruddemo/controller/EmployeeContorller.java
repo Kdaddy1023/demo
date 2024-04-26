a119753ffcbf99941c62e9465b0fe3ea164384c6
Kdaddy1023 <zjlikai1023@outlook.com>
2024-04-26 13:20:04 +0800

    test

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeContorller.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeContorller.java
@@ -4,8 +4,7 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +23,47 @@ public class EmployeeContorller {
 
         theModel.addAttribute("employees",theEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId,Model theModel){
+        Employee theEmployee = employeeService.findById(theId);
+
+        theModel.addAttribute("employee",theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+        employeeService.deleteById(theId);
+
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String getTest() {
+        // 将Employee对象转换为字符串表示形式
+        Employee theEmployee = employeeService.findById(1);
+        return theEmployee.getEmail();
+    }
+
 }
